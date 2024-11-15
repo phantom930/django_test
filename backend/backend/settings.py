68d1a396161ad6965c38c0802033f7dfc66cc592
phatom930 <phantom93930@gmail.com>
2024-11-15 00:06:19 -0500

    feat(backend): admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'coresheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
