f6894be70a9ed0c0bd49589bdab74ceadb5ea170
phatom930 <phantom93930@gmail.com>
2024-11-15 04:07:10 -0500

    feat(frontend): product list

--- frontend/src/app/actions.ts
+++ frontend/src/app/actions.ts
@@ -5,22 +5,32 @@ interface LoginResponse {
   token: string
 }
 
-interface Product {
+export interface Product {
   id: number;
   name: string;
   description: string;
   price: number;
-  stoke: number;
+  stock: number;
 }
 
 // Create Axios Instance
 const API = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'http://127.0.0.1:8000'
 });
 
 // Login function with typed parameters and return type
 export const login = async (username: string, password: string): Promise<LoginResponse> => {
   const response = await API.post<LoginResponse>('/authentication/login', { username, password });
   localStorage.setItem('token', response.data.token);
+  return response.data;
+};
+
+// Fetch products function with typed query parameter and return type
+export const fetchProducts = async (query: string): Promise<Product[]> => {
+  const response = await API.get<Product[]>(`/products/list?query=${query}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem('token')}`,
+    },
+  });
   return response.data;
 };
